222393f89ba4e601d0a3187533b82a61eb7a81f0
Jijie Chen <jijie.chen@someplace.com>
2019-06-02 22:22:26 +0800

    升级了 .NET Core 和相关依赖项的版本

--- src/StatlerWaldorfCorp.EventProcessor/Queues/AMQP/AMQPConnectionFactory.cs
+++ src/StatlerWaldorfCorp.EventProcessor/Queues/AMQP/AMQPConnectionFactory.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.Options;
 using RabbitMQ.Client;
+using System;
 using System.Linq;
 using Microsoft.Extensions.Logging;
 using StatlerWaldorfCorp.EventProcessor.Models;
@@ -20,7 +21,7 @@ namespace StatlerWaldorfCorp.EventProcessor.Queues.AMQP
             this.Password = amqpOptions.Password;
             this.VirtualHost = amqpOptions.VirtualHost;
             this.HostName = amqpOptions.HostName;
-            this.Uri = amqpOptions.Uri;
+            this.Uri = new Uri(amqpOptions.Uri);
 
             logger.LogInformation($"AMQP Connection configured for URI : {amqpOptions.Uri}");
         }
